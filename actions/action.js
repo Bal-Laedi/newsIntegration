ffa486bab936a6fd19bdc9fbb1a14fd0402835ee
shung-yang <wl03154316@gmail.com>
2020-06-22 00:45:02 +0800

    Add action

---
+++ actions/action.js
@@ -0,0 +1,16 @@
+//Projects
+export const toggleNewSource = filter => ({
+	type: 'TOGGLE_NEWS_SOURCE',
+	filter	
+})
+
+export const addNews = news => ({
+	type: 'ADD_NEWS',
+	news
+})
+
+//Project
+/*export const toggleLike = like => (
+	type: 'TOGGLE_LIKE',
+	like
+})*/

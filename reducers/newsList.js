7a1fe015564692622f7675d379252acfec2e264f
shung-yang <wl03154316@gmail.com>
2020-06-22 00:48:30 +0800

    create reducer "newsList" to replace state "testArr" in component "Projects"

---
+++ reducers/newsList.js
@@ -0,0 +1,22 @@
+const newsList = (state = [], action) => {
+	switch(action.type) {
+		case 'ADD_NEWS':
+			return [
+				...state,
+				{
+					'id': action.news.id,
+					'href': action.news.href,
+					'image': action.news.image,
+					'title': action.news.title,
+					'date': action.news.date,
+					'data_src': action.news.data_src
+				}
+			]
+		default:
+			return state
+	}	
+}
+
+
+
+export default newsList;

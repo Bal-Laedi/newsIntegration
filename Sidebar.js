85b497d3415f8382b78ad55a6e2141f19a2dbf6a
Wilson <wl03154316@gmail.com>
2020-07-09 03:15:19 +0800

    Sidebar basic style
    Except hover style, other Sidebar style is done

--- Sidebar.js
+++ Sidebar.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import db from './initial_firebase';
 import { connect } from 'react-redux';
 import { toggleNewSource, addDataSource, emptyNews} from './actions/action';
+import './Sidebar.css';
 
 class Sidebar_item extends Component{
 	constructor(props){
@@ -9,23 +10,25 @@ class Sidebar_item extends Component{
 	}
 
 	render(){
-		let checkMark;
+		let chooseMark;
+		let fontColor;
 		if(this.props.check){
-        	checkMark = 
-        		<svg className="svg-icon" width="24" height="24" viewBox="0 0 20 20">
-					<path fill="green" d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>						
+        	chooseMark = 
+        		<svg className="svg-icon"  viewBox="0 0 20 20" choose="true"  style = {{ fill : "#DBA513" }}>						
+					<line x1="8" y1="0" x2="8" y2="20"/>
 				</svg>
+			fontColor = "#DBA513";
 		}
         else{
-        	checkMark = 
-        		<svg className="svg-icon" width="0" height="0" viewBox="0 0 20 20">
-					<path fill="green" d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>						
+        	chooseMark = 
+        		<svg className="svg-icon" width="0" height="0" viewBox="0 0 20 20" choose="false">							
 				</svg>
+			fontColor = "#FFF";
         }
 		return(
-			<div className="Sidebar_item" collection={this.props.collection} onClick={this.props.toggleNewSource}>
-        		{this.props.data_source}
-        		{checkMark}
+			<div className="Sidebar_item" collection={this.props.collection} onClick={this.props.toggleNewSource} style = {{ color : fontColor}}>
+        		{chooseMark}
+        		<div>{this.props.data_source}</div>
         	</div>
 		)
 	}
@@ -63,9 +66,8 @@ class Sidebar extends Component{
 			<div id="offcanvas-overlay" data-uk-offcanvas="overlay: true">
     			<div className="uk-offcanvas-bar">
         			<button className="uk-offcanvas-close" type="button" data-uk-close></button>
-        			<h3>新聞來源</h3>
+        			<div className='Sidebar-title'>Sources</div>
         			{	
-						//this.state.data_source_arr.map((doc) => {
 						this.props.data_source_arr.map((doc) => {	
     						return(
 								<ChooseDataSrc key={doc.id} id={doc.id} collection={doc.collection} data_source={doc.name}/>

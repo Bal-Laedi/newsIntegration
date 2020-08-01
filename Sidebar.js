8fefb2be5b490540b42e27986621ac7a1727b414
macbook <wl03154316@gmail.com>
2020-08-02 03:26:08 +0800

    Sidebar item hover 範圍調整

--- Sidebar.js
+++ Sidebar.js
@@ -30,14 +30,14 @@ class Sidebar_item extends Component{
 		if(this.props.check){
 			if(!this.state.hover){
         		chooseMark = 
-        			<svg className="svg-icon"  viewBox="0 0 20 20" style = {{ fill : "#DBA513" }}>						
+        			<svg className="svg-icon" onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} viewBox="0 0 20 20" style = {{ fill : "#DBA513" }}>						
 						<line x1="10" y1="0" x2="10" y2="20"/>
 					</svg>
 				fontColor = "#DBA513";
 			}
 			else{
 				chooseMark = 
-        			<svg className="svg-icon" viewBox="0 0 25 25">
+        			<svg className="svg-icon" onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} viewBox="0 0 25 25">
 						<path fill="#D8BD71" d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
 					</svg>
 				fontColor = "#D8BD71";
@@ -46,13 +46,13 @@ class Sidebar_item extends Component{
         else{
         	if(!this.state.hover){
         		chooseMark = 
-        			<svg className="svg-icon" width="0" height="0" viewBox="0 0 20 20">							
+        			<svg className="svg-icon" onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} width="0" height="0" viewBox="0 0 20 20">							
 					</svg>
 				fontColor = "#FFF";
 			}
 			else{
 				chooseMark =
-					<svg className="svg-icon" viewBox="0 0 20 20">
+					<svg className="svg-icon" onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} viewBox="0 0 20 20">
 						<path fill="#D8BD71" d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"></path>
 					</svg>
 				fontColor = "#D8BD71";
@@ -65,9 +65,9 @@ class Sidebar_item extends Component{
        	}
 
 		return(
-			<div className="Sidebar_item" collection={this.props.collection} onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} style = {{ color : fontColor}}>
+			<div className="Sidebar_item" collection={this.props.collection} style = {{ color : fontColor}}>
         		{chooseMark}
-        		<div>{this.props.data_source}</div>
+        		<div className="news-source" onClick={this.toggleCheck} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>{this.props.data_source}</div>
         	</div>
 		)
 	}
